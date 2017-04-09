672cb32726d881de6167a96c9db0608706a4305f
metalmount <sokunbitaiwo82@gmail.com>
2017-04-09 11:16:51 +0100

    make correction to aritGeo app

--- spec/aritGeo.js
+++ spec/aritGeo.js
@@ -1,45 +1,69 @@
-function gcd(m,n){
-    while(m%n != 0){
-       var oldm = m ; 
-       var oldn = n ;
+function arithmetic(num){
+  var arithmetic = false;
+  const c = num[1] - num[0];
+  for(var i=0 ; i<num.length; i++){
+    for(var j=i+1; j<num.length; j++){
+      if(num[j] === c+ num[j-1]){
+        arithmetic=true;
+  		}
+  		else{
+  		  arithmetic=false;
+  		  break;
+  		}
 
-       var m = oldn ; 
-       var n = oldm%oldn ; 
-    }
-    return(n);
-
-
+  
+  
+  	}
+  		
+  }
+  return(arithmetic);
 }
+
+function geometric(num){
+  const r = num[1] /num[0];
+	const a = num[0];
+  var geometric = false;
+  for(var i=0 ; i<num.length; i++){
+    for(var j=i+1; j<num.length; j++){
+      if(num[j] === a*(Math.pow(r,j))){
+        geometric=true;
+  		}
+  		else{
+  		  geometric=false;
+  		  break;
+  		}
+
+  
+  
+  	}
+  		
+  }
+  return(geometric);
+}
+
 
 var exports =module .exports = {};
 
 exports.aritGeo=function(num){
-	
 	if(num.length === 0){
-		return(0);
+		return(0)
+	}else if(geometric(num)){
+	  return("Geometric");
+	}else if(arithmetic(num)){
+	  return("Arithmetic");
 	}
 	else{
-		for(var i=0 ; i<num.length; i++){
-			for(var j=i+1; j<num.length; j++){
-				if(num[j]-num[i] === num[j+1] - num[j]){
-					return("Arithmetic");
-				}
-				else if(gcd(num[j],num[i])=== gcd(num[j+i],num[j])){
-					return("Geometric");
-				}
-				else{
-					return(-1);
-			    
-			  	}
-
-
-			}
-		}
-
+	  return(-1)
 	}
 
-
 };
+
+
+
+
+
+
+
 
 
 
